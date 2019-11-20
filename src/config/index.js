28bd039efec7155cef3bfd13a1da36431fae6b3a
lotosbin <lotosbin@gmail.com>
2019-11-20 15:40:53 +0800

    fix: fix config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,10 @@
-const result = require('dotenv').config({debug: process.env.DEBUG});
-if (result.error) {
-    throw result.error
+if (process.env.NODE_ENV !== 'production') {
+    const result = require('dotenv').config({debug: process.env.DEBUG});
+    if (result.error) {
+        throw result.error
+    }
+    console.log(result.parsed);
 }
-console.log(result.parsed);
-
 const config = {
     API_URL: process.env.REACT_APP_API_URL || ""
 };
